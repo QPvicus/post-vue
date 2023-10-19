c9072e47c5629746e3abf8d53ed061fc98750593
QPvicus <57519939+QPvicus@users.noreply.github.com>
2023-10-19 09:05:01 +0800

    feat: define tab-area

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,16 +1,17 @@
 <script lang="ts" setup>
 import ComponentsList from '@/components/editor/components-list.vue'
+import TabArea from '@/components/editor/tab-area.vue'
 import { useUserStore } from '@/stores'
-import { computed } from 'vue';
+import { computed } from 'vue'
 const userStore = useUserStore()
 const userInfo = computed(() => userStore.user)
 </script>
 
 <template>
-  <div class="editor">
-    <a-drawer placement="right" :width="400" title="设置面板">
+  <div class="editor" id="editor-layout-main">
+    <!-- <a-drawer placement="right" :width="400" title="设置面板">
       <p>内容1.。。。</p>
-    </a-drawer>
+    </a-drawer> -->
     <a-layout>
       <a-layout-header class="header">
         <div class="page-title">头部header</div>
@@ -32,24 +33,139 @@ const userInfo = computed(() => userStore.user)
     </a-layout>
     <a-layout>
       <a-layout-sider width="300" style="background: #fff">
-        <components-list> </components-list>
+        <div class="sidebar-container">
+          <components-list> </components-list>
+        </div>
       </a-layout-sider>
       <a-layout style="padding: 0 24px 24px">
-        <a-layout-content> 画布区域 </a-layout-content>
+        <a-layout-content class="preview-container">
+          <p>画布区域</p>
+          <tab-area></tab-area>
+        </a-layout-content>
       </a-layout>
-      <a-layout-sider width="300" style="background: #fff"> 右侧布局 </a-layout-sider>
+      <a-layout-sider width="300" style="background: #fff" class="settings-panel">
+        右侧布局
+      </a-layout-sider>
     </a-layout>
   </div>
 </template>
 
 <style>
-
 .header {
   display: flex;
   justify-content: space-between;
 }
-
+.header .logo-img {
+  margin-right: 20px;
+  height: 40px;
+}
 .page-title {
   display: flex;
+}
+.header h4 {
+  color: #ffffff;
+}
+.editor-spinner {
+  position: fixed;
+  right: 50%;
+  top: 10px;
+}
+.preview-container {
+  padding: 24px;
+  margin: 0;
+  min-height: 90vh !important;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+}
+.preview-list {
+  padding: 0;
+  margin: 0;
+  min-width: 375px;
+  min-height: 200px;
+  border: 1px solid #efefef;
+  background: #fff;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: fixed;
+  margin-top: 50px;
+  max-height: 80vh;
+}
+.preview-list.active {
+  border: 1px solid #1890ff;
+}
+.preview-list.canvas-fix .l-text-component,
+.preview-list.canvas-fix .l-image-component,
+.preview-list.canvas-fix .l-shape-component {
+  box-shadow: none !important;
+}
+.preview-list.canvas-fix {
+  position: absolute;
+  max-height: none;
+}
+.sidebar-container {
+  padding: 20px;
+}
+.body-container {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+}
+.page-settings {
+  padding: 16px;
+}
+.settings-panel .ant-tabs-top-content {
+  max-height: calc(100vh - 68px - 56px);
+  overflow-y: auto;
+}
+.final-preview {
+  position: absolute;
+  width: calc(100% - 400px);
+  height: 100%;
+  background: transparent;
+  top: 0;
+  left: 0;
+  z-index: 1500;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.final-preview-inner {
+  width: 430px;
+  height: 870px;
+  padding: 60px 28px;
+  position: relative;
+  background: url('@/assets/phone-back.png') no-repeat;
+  background-size: cover;
+}
+.final-preview-inner .preview-title {
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  font-weight: bold;
+}
+.iframe-container {
+  width: 100%;
+  height: 706px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+.iframe-placeholder {
+  background: url('@/assets/loading.svg') 50% 50% no-repeat;
+  background-size: 50px;
+}
+.settings-panel .ant-list-bordered {
+  border-radius: 0;
+}
+.settings-panel .ant-collapse {
+  border-radius: 0;
+}
+.ant-collapse-header,
+.ant-collapse-item {
+  border-radius: 0 !important;
+}
+.settings-panel .ant-tabs-tab {
+  border-radius: 0 !important;
 }
 </style>
